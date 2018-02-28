4c3fb2db26ef2b4dd231a2ca4cc7460652bd3f44
aussieW <austin.j.wilson@gmail.com>
2018-03-01 05:17:59 +1100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from mycroft.util.log import getLogger
+from mycroft.skills.core import MycroftSkill
+import subprocess
+from os.path import join, abspath
+
+__author__ = 'aussieW'
+
+LOGGER = getLogger(__name__)
+
+class MycroftPushToListen(MycroftSkill):
+    def __init__(self):
+        super(MycroftPushToListen, self).__init__(name='MycroftPushToListen')
+        
+    def initialize(self):
+        # start the button listener
+        subprocess.run(join(abspath(dirname(__file__), 'button.py')
+        
+def create_skill():
+    return MycroftPushToListen()
