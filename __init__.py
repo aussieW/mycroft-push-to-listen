a09d837d88718f423440906575952c06a8982bb6
aussieW <austin.j.wilson@gmail.com>
2018-03-01 17:36:29 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,17 +10,18 @@ LOGGER = getLogger(__name__)
 class MycroftPushToListen(MycroftSkill):
     def __init__(self):
         super(MycroftPushToListen, self).__init__(name='MycroftPushToListen')
-        buttonPin = self.settings['gpio']
+        self.button_pin = self.settings['gpio']
+        self.proc = None
         
     def initialize(self):
         # start the button listener
         #subprocess.call(join(abspath(dirname(__file__)), 'button.py'))
-        proc = subprocess.Popen(['python', '/opt/mycroft/skills/mycroft-push-to-listen/button.py', buttonPin])
-        LOGGER.info('button process pid = ' + str(proc.pid))
+        self.proc = subprocess.Popen(['python', '/opt/mycroft/skills/mycroft-push-to-listen/button.py', self.button_pin])
+        LOGGER.info('button process pid = ' + str(self.proc.pid))
         
     def shutdown(self):
         # shutdown the button.py process
-        proc.kill()
+        self.proc.kill()
         super(MycroftPushToListen, self).shutdown()
         
 def create_skill():
