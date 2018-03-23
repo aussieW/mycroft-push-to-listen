a5ad5fb8dc70197949104e4da6feb13b9d396925
aussieW <austin.j.wilson@gmail.com>
2018-03-23 12:25:34 +1100

    Added callback to handle a change to the GPIO pin

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft.util.log import getLogger
 from mycroft.skills.core import MycroftSkill
 import subprocess
+import time
 #from os.path import join, abspath, dirname
 
 __author__ = 'aussieW'
@@ -13,16 +14,35 @@ class MycroftPushToListen(MycroftSkill):
         self.button_pin = self.settings['gpio']
         self.proc = None
         
+        # listen for setting changes
+        self.set_changed_callback(self._check_gpio_changed)
+        
     def initialize(self):
         # start the button listener
         #subprocess.call(join(abspath(dirname(__file__)), 'button.py'))
+        self._start()
+        
+    def _check_gpio_changed(self):
+        # check if the gpio pin has changed
+        if self.settings['gpio'] <> self.button_pin:
+            LOGGER.info("GPIO pin changed")
+            # restart button.py with the new pin assigned
+            self._stop()
+            time.sleep(2)  # chose an arbitrary value
+            self.button_pin = self.settings['gpio']
+            self.start()
+        
+    def _start(self):
         self.proc = subprocess.Popen(['python', '/opt/mycroft/skills/mycroft-push-to-listen/button.py', self.button_pin])
         LOGGER.info('button process pid = ' + str(self.proc.pid))
+    
+    def _stop(self):
+        self.proc.kill()
+        LOGGER.info('Shutting down button.py')
         
     def shutdown(self):
         # shutdown the button.py process
-        LOGGER.info('Shutting down button.py')
-        self.proc.kill()
+        self._stop()
         super(MycroftPushToListen, self).shutdown()
         
 def create_skill():
